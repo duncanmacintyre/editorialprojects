427aa3609168142489cb9589c1a5a3e8f4b6328b
duncanmacintyre <duncanmacintyre@users.noreply.github.com>
2017-05-15 12:32:26 -0700

    Create Insert Taskpaper Project from Template.py

---
+++ Insert Taskpaper Project from Template.py
@@ -0,0 +1,53 @@
+# Insert Taskpaper Project from Template
+# A workflow for the iOS text editor Editorial
+# Copyright (c) 2017 Duncan MacIntyre
+
+# It is recommended to have an understanding of taskpaper before continuing:
+# http://omz-software.com/editorial/docs/ios/editorial_writing_todo_lists.html
+
+# This script will allow the user to define taskpaper "templates" for projects, set in 
+# /Task Management/Templates.taskpaper in Dropbox. (Their syntax is the same as for regular taskpaper
+# projects.) "Variables" can be defined inside of {}
+
+# some terms used in comments:
+# - the Templates.taskpaper file is defined as /Task Management/Templates.taskpaper in Dropbox
+# - a project is defined as a taskpaper project in Templates.taskpaper
+# - a project name is defined as the first line of a taskpaper project, minus the :
+# - a project's contents are defined as the whole project, including the first line, tasks, and notes
+# - a variable is defined as {variable name} in the Templates.taskpaper file, and the text that will
+#   eventually replace this
+# - a variable name is defined as the text between { and } for each variable defined in the
+#   Templates.taskpaper file
+
+import re
+import editor
+import dialogs
+
+# the following code:
+# 1. gets the contents of the Templates.taskpaper file
+# 2. uses a regular expression to find all occurences of a project
+# 3. creates a dictionary called templates with items <project name>:<project contents>
+templates = {i[1]:i[0] for i in re.findall('((.+):(?:\n.+)+)', editor.get_file_contents('/Task Management/Templates.taskpaper', 'dropbox'))}
+
+# the following code:
+# 1. gets the project names (the keys in the templates dictionary)
+# 1. uses Editorial's built-in dialogs module to ask the user to choose from a list of these
+# 2. gets the project contents corresponding to the chosen project name
+#    (the value relating to the chosen key)
+# 3. stores this under chosenTemplate
+chosenTemplate = templates[dialogs.list_dialog(title = 'Choose a template:', items = templates.keys())]
+
+# set up a list to hold variable names
+# (the user will be asked to enter values for these)
+variableNames = []
+# use a regular expression to find each variable in the template contents
+for i in re.finditer('{(.+?)}', chosenTemplate):
+	# unless it has already been done, append each variable name to the variableNames list
+	if i.group(1) not in variableNames: variableNames.append(i.group(1))
+
+# the following code:
+# 1. uses Editorial's built-in dialogs module to ask the user to fill out a form, where
+#    there is one text field for each variable found in the variableNames list
+# 2. uses str.format() to insert the variable values given by the user into the template
+# 3. inserts the resulting text into the open document
+editor.insert_text(chosenTemplate.format(**dialogs.form_dialog(title='Enter values for the following variables:', fields=[{'type':'text', 'key':i, 'title':i} for i in variableNames]) if len(variableNames) > 0 else {}))
